a94cad4982cd12c883b89a5344838e558209a305
xiaolinzi7118 <1106147743@qq.com>
2026-04-27 15:41:54 +0800

    添加mock数据和图片

--- track-h5-demo/src/main.js
+++ track-h5-demo/src/main.js
@@ -19,3 +19,22 @@ if (window.TrackSDK) {
     }
   })
 }
+
+// mock 用户信息和系统信息
+window.userInfo = {
+  "id": 1,
+  "username": "admin",
+  "password": null,
+  "nickname": "管理员",
+  "avatar": "",
+  "status": 1,
+  "createTime": "2026-04-08T17:20:29.344184",
+  "updateTime": "2026-04-08T17:20:29.344184"
+}
+
+window.system = {
+  os: 'iOS',
+  browser: 'Safari',
+  device: 'iPhone',
+  appVersion: '1.0.0'
+}
